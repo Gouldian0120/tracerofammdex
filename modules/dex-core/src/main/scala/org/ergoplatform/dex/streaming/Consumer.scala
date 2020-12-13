bb743b7f1b0967a61e10a2deba507f371d767936
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2020-12-13 22:53:01 +0300

    Unspent boxes tracking impl using explorer.

--- modules/dex-core/src/main/scala/org/ergoplatform/dex/streaming/Consumer.scala
+++ modules/dex-core/src/main/scala/org/ergoplatform/dex/streaming/Consumer.scala
@@ -13,34 +13,32 @@ import tofu.higherKind.Embed
 import tofu.syntax.context._
 import tofu.syntax.monadic._
 
-import scala.annotation.unchecked.uncheckedVariance
-
 trait Consumer[K, V, F[_], G[_]] {
 
-  type O
+  type Offset
 
-  def stream: F[Committable[K, V, O, G]]
+  def stream: F[Committable[K, V, Offset, G]]
 }
 
 object Consumer {
 
-  type Aux[K, V, O1, F[_], G[_]] = Consumer[K, V, F, G] { type O = O1 }
+  type Aux[K, V, O, F[_], G[_]] = Consumer[K, V, F, G] { type Offset = O }
 
-  final private class ConsumerContainer[K, V, F[_]: FlatMap, G[_]](
-    ffa: F[Consumer[K, V, F, G]]
+  final private class ConsumerContainer[K, V, O1, F[_]: FlatMap, G[_]](
+    ffa: F[Consumer.Aux[K, V, O1, F, G]]
   ) extends Consumer[K, V, F, G] {
 
-    type O = Any
+    type Offset = O1
 
-    def stream: F[Committable[K, V, O, G]] =
-      ffa.flatMap(_.stream.widen[Committable[K, V, O, G]])
+    def stream: F[Committable[K, V, Offset, G]] =
+      ffa.flatMap(_.stream)
   }
 
-  implicit def embed[I[_], K, V]: Embed[Consumer[K, V, *[_], I]] =
-    new Embed[Consumer[K, V, *[_], I]] {
+  implicit def embed[I[_], K, V, O]: Embed[Consumer.Aux[K, V, O, *[_], I]] =
+    new Embed[Consumer.Aux[K, V, O, *[_], I]] {
 
-      def embed[F[_]: FlatMap](ft: F[Consumer[K, V, F, I]]): Consumer[K, V, F, I] =
-        new ConsumerContainer[K, V, F, I](ft)
+      def embed[F[_]: FlatMap](ft: F[Consumer.Aux[K, V, O, F, I]]): Consumer.Aux[K, V, O, F, I] =
+        new ConsumerContainer[K, V, O, F, I](ft)
     }
 
   implicit def functorK[I[_], K, V]: FunctorK[Consumer[K, V, *[_], I]] =
@@ -48,8 +46,8 @@ object Consumer {
 
       def mapK[F[_], G[_]](af: Consumer[K, V, F, I])(fk: F ~> G): Consumer[K, V, G, I] =
         new Consumer[K, V, G, I] {
-          type O = af.O
-          def stream: G[Committable[K, V, O, I]] = fk(af.stream)
+          type Offset = af.Offset
+          def stream: G[Committable[K, V, Offset, I]] = fk(af.stream)
         }
     }
 
@@ -60,16 +58,17 @@ object Consumer {
     V
   ](implicit makeConsumer: MakeKafkaConsumer[G, K, V]): Consumer[K, V, F, G] =
     embed.embed(
-      context map (conf => functorK.mapK(new Live[K, V, G](conf))(LiftStream[F, G].liftF))
+      (context map (conf => functorK.mapK(new Live[K, V, G](conf))(LiftStream[F, G].liftF)))
+        .asInstanceOf[F[Consumer.Aux[K, V, Live[K, V, F]#Offset, F, G]]]
     )
 
   final class Live[K, V, F[_]: Functor](config: ConsumerConfig)(implicit
     makeConsumer: MakeKafkaConsumer[F, K, V]
   ) extends Consumer[K, V, Stream[F, *], F] {
 
-    type O = (TopicPartition, OffsetAndMetadata)
+    type Offset = (TopicPartition, OffsetAndMetadata)
 
-    def stream: Stream[F, Committable[K, V, O, F]] =
+    def stream: Stream[F, Committable[K, V, Offset, F]] =
       makeConsumer(config)
         .evalTap(_.subscribeTo(config.topicId.value))
         .flatMap(_.stream)
