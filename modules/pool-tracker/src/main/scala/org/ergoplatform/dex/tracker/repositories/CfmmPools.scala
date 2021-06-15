b97a8d158b2379a574d9ae07ee0580c398ea0dc3
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2021-06-15 13:09:27 +0300

    CFMM pools tracking.

--- modules/pool-tracker/src/main/scala/org/ergoplatform/dex/tracker/repositories/CfmmPools.scala
+++ modules/pool-tracker/src/main/scala/org/ergoplatform/dex/tracker/repositories/CfmmPools.scala
@@ -2,41 +2,41 @@ package org.ergoplatform.dex.tracker.repositories
 
 import cats.Monad
 import org.ergoplatform.dex.domain.{OnChain, Predicted}
-import org.ergoplatform.dex.domain.amm.{CfmmPool, PoolId}
+import org.ergoplatform.dex.domain.amm.{CFMMPool, PoolId}
 import org.ergoplatform.ergo.ErgoNetwork
 
-trait CfmmPools[F[_]] {
+trait CFMMPools[F[_]] {
 
   /** Get actual pool state by pool id.
     */
-  def getOnChain(id: PoolId): F[Option[OnChain[CfmmPool]]]
+  def getOnChain(id: PoolId): F[Option[OnChain[CFMMPool]]]
 
   /** Put actual pool state.
     */
-  def putOnChain(pool: OnChain[CfmmPool]): F[Unit]
+  def putOnChain(pool: OnChain[CFMMPool]): F[Unit]
 
   /** Get latest predicted pool state by pool id.
     */
-  def getPredicted(id: PoolId): F[Option[Predicted[CfmmPool]]]
+  def getPredicted(id: PoolId): F[Option[Predicted[CFMMPool]]]
 
   /** Put predicted pool state.
     */
-  def putPredicted(pool: Predicted[CfmmPool]): F[Unit]
+  def putPredicted(pool: Predicted[CFMMPool]): F[Unit]
 }
 
-object CfmmPools {
+object CFMMPools {
 
-  def make[F[_]: Monad](implicit network: ErgoNetwork[F]): CfmmPools[F] =
+  def make[F[_]: Monad](implicit network: ErgoNetwork[F]): CFMMPools[F] =
     new Live[F]
 
-  final class Live[F[_]: Monad](implicit network: ErgoNetwork[F]) extends CfmmPools[F] {
+  final class Live[F[_]: Monad](implicit network: ErgoNetwork[F]) extends CFMMPools[F] {
 
-    def getOnChain(id: PoolId): F[Option[OnChain[CfmmPool]]] = ???
+    def getOnChain(id: PoolId): F[Option[OnChain[CFMMPool]]] = ???
 
-    def putOnChain(pool: OnChain[CfmmPool]): F[Unit] = ???
+    def putOnChain(pool: OnChain[CFMMPool]): F[Unit] = ???
 
-    def getPredicted(id: PoolId): F[Option[Predicted[CfmmPool]]] = ???
+    def getPredicted(id: PoolId): F[Option[Predicted[CFMMPool]]] = ???
 
-    def putPredicted(pool: Predicted[CfmmPool]): F[Unit] = ???
+    def putPredicted(pool: Predicted[CFMMPool]): F[Unit] = ???
   }
 }
